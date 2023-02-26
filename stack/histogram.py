4a45be97479ecd0c4ed19fa4ced1a1642b05aea9
Shruti jain <65339232+shruti463@users.noreply.github.com>
2023-02-26 19:08:30 +0530

    Update histogram.py
    Signed-off-by: Shruti jain <65339232+shruti463@users.noreply.github.com>

--- stack/histogram.py
+++ stack/histogram.py
@@ -20,4 +20,30 @@ def histo(arr):
 print(histo([6,2,5,4,1,5,6]))
 
 
-#efficient approach
+#efficient approach(TLE)
+def NSL(arr , i):
+    for j in  range (i ,-1,-1):
+        if arr[j]< arr[i]:
+            return j+1
+    return 0
+
+def NSR(arr,i):
+    for j in  range (i , len(arr)):
+        if arr[j]< arr[i]:
+            return j-1
+    return len(arr)-1
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        arr = heights
+        n = len(arr)
+        res = 0
+        for i in range(n):
+            a = NSL(arr,i)
+            b = NSR(arr, i)
+
+            res = max(res, (b-a+1)*arr[i])
+        return res
+    
+
+        
